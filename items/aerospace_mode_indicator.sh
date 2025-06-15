54227f4a09e14212e16a3089b2101943d7d22320
Felix Ding <felixding1989@gmail.com>
2025-06-14 19:25:05 -0700

    adds icons for aerospace modes

--- items/aerospace_mode_indicator.sh
+++ items/aerospace_mode_indicator.sh
@@ -2,7 +2,7 @@
 
 sketchybar --add item aerospace_mode left \
   --subscribe aerospace_mode aerospace_mode_change \
-  --set aerospace_mode icon="" \
+  --set aerospace_mode \
   padding_left=10 \
   script="$CONFIG_DIR/plugins/aerospace_mode.sh" \
   icon.color="$ACCENT_COLOR" \
--- plugins/aerospace_mode.sh
+++ plugins/aerospace_mode.sh
@@ -5,7 +5,16 @@ CURRENT_MODE=$(aerospace list-modes --current)
 if [ "$CURRENT_MODE" == "main" ]; then
   sketchybar --set "$NAME" \
     drawing=off
-else
+elif [ "$CURRENT_MODE" == "jump" ]; then
   sketchybar --set "$NAME" \
+    icon="⇄" \
+    drawing=on
+elif [ "$CURRENT_MODE" == "move" ]; then
+  sketchybar --set "$NAME" \
+    icon="●" \
+    drawing=on
+elif [ "$CURRENT_MODE" == "service" ]; then
+  sketchybar --set "$NAME" \
+    icon="" \
     drawing=on
 fi
